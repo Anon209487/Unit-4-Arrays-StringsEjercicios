8ffb978151b655c6d2025207b6ba6749bc9eb3ca
Anon209487 <156091669+Anon209487@users.noreply.github.com>
2024-04-08 14:31:41 +0200

    asd

--- EjerciciosArrays/src/boletin4/Act1.java
+++ EjerciciosArrays/src/boletin4/Act1.java
@@ -14,6 +14,6 @@ public class Act1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
 
+}
 }
---
+++ EjerciciosArrays/src/boletin6/ExamenString.java
@@ -0,0 +1,99 @@
+/**
+ * 
+ */
+package boletin6;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class ExamenString {
+	public static String[] palabras = { "humanidad", "persona", "hombre", "mujer", "individuo", "cuerpo", "pierna",
+			"cabeza", "brazo", "familia" };
+	final static int NUMINTENTOS = 7;
+	static String palabraSecreta = "";
+	static String palabraPista = "";
+	static String noAcertadas = "";
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int intententos = 0;
+		generaPalabra();
+		pintaPista();
+		while (intententos < NUMINTENTOS) {
+
+			intententos = menu() + intententos;
+
+		}
+		System.out.println("Has superado el numero de intentos...");
+	}
+
+	public static void generaPalabra() {
+		// TODO Auto-generated method stub
+		int aleatorio = (int) (Math.floor(Math.random() * (0 + palabras.length)));
+		palabraSecreta = palabras[aleatorio];
+
+	}
+
+	public static void pintaPista() {
+		// TODO Auto-generated method stub
+		String pista = "";
+		for (int i = 0; i < palabraSecreta.length(); i++) {
+			pista = pista + " " + "*";
+		}
+		System.out.println(pista);
+	}
+
+	public static int menu() {
+		// TODO Auto-generated method stub
+		String letra = "";
+		int opcion = 0;
+		String palabra = "";
+
+		System.out.println(" Seleccione una de las siguientes opciones:");
+		System.out.println("1: Introducir letra.");
+		System.out.println("2: Introducir palabra.");
+		opcion = extracted().nextInt();
+		switch (opcion) {
+		case 1: {
+			System.out.println(" Introducir letra.");
+			letra = extracted().nextLine();
+			if (palabra != null) {
+				compruebaLetra(letra);
+			}
+			break;
+		}
+		case 2: {
+			System.out.println("Introducir palabra.");
+			palabra = extracted().nextLine();
+			if (palabra != null && !palabra.equals("")) {
+				compruebaPalabra(palabra);
+			}
+			break;
+		}
+		}
+		return 7;
+
+	}
+
+	public static void compruebaLetra(String letra) {
+		// TODO Auto-generated method stub
+	}
+
+	public static void compruebaPalabra(String palabra) {
+		// TODO Auto-generated method stub
+		if (palabra.equalsIgnoreCase(palabraSecreta)) {
+			System.out.println("HAS ACERTADO!");
+
+		}
+	}
+
+	public static Scanner extracted() {
+		return new Scanner(System.in);
+	}
+
+}
